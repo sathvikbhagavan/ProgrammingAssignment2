9c94f41dd2464287ed666eac92fd9f4fca8fff8d
sathvikbhagavan <sathvikbhagavan@gmail.com>
2020-07-06 14:15:44 +0530

    Completed Assignment-2

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
-# This function helps in making cache/separate object such that if a matrix in its parent environment is used 
-# for calculating its inverse again and again, instead of calculating is again and again, we can look up if its 
-# calculated once and using its value anytime we want
+# This function helps in making cache/separate objects using the concept of lexical scoping 
+# such that if a matrix existing its parent environment is used 
+# for calculating its inverse again and again, instead of calculating it again and again, we can look up if its 
+# inverse which would have been calculated once and stored and hence we can use its value anytime we want
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
     set <- function(y) {
@@ -23,8 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-# This is the driver function for calculating the inverse. If the inverse of a particular matrix object has been 
-# calculated, it can be directly retrieved without need for calculating again which can be useful for saving computational
+# This is the driver function for calculating the inverse. First, we cache the matrix and then we call 
+# this function passing the return value of makeCacheMatrix for calculating its inverse
+# If the inverse of a particular matrix object has been 
+# calculated before, it can be directly retrieved without need for calculating again which can be useful for saving computational
 # time.
 cacheSolve <- function(x, ...) {
     inverse <- x$getInverse()
