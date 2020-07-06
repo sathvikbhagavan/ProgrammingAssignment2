e7bac4a226e243140d9dccaf6648fa67c9b37e4d
sathvikbhagavan <sathvikbhagavan@gmail.com>
2020-07-06 21:28:36 +0530

    Example Included

--- cachematrix.R
+++ cachematrix.R
@@ -8,6 +8,12 @@
 ## such that if a matrix existing its parent environment is used 
 ## for calculating its inverse again and again, instead of calculating it again and again, we can look up if its 
 ## inverse which would have been calculated once and stored and hence we can use its value anytime we want
+
+## It consists:
+## 1. set() - set the value of the matrix object
+## 2. get() - get the value of the matrix object
+## 3. getInverse() - get the value of inverse of the matrix object if cached
+## 4. setInverse() - set the value of the inverse of the matrix object if not cached for future references
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
     set <- function(y) {
@@ -44,3 +50,16 @@ cacheSolve <- function(x, ...) {
     x$setInverse(inverse)
     return(inverse)
 }
+
+
+## Usage:
+
+# Make cached object (which is a list of functions) of a matrix object
+cache_m1 <- makeCacheMatrix(matrix(c(1,4,3,2), nrow = 2, ncol = 2))
+# As inverse is not cached the first time it will calculated and set
+cacheSolve(cache_m1)
+# We can directly use the cache
+cacheSolve(cache_m1)
+# We can change the cached matrix object:
+cache_m1$set(matrix(c(3,2,1,4), nrow = 2, ncol = 2))
+# We can then again use it for inverse matrix calculations
